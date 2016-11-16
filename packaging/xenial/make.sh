865b46ba1426c107061f30cea0187552a42dd6e1
Kohei Suzuki <eagletmt@gmail.com>
2016-11-16 22:50:16 +0900

    Fix comment

--- packaging/xenial/make.sh
+++ packaging/xenial/make.sh
@@ -14,11 +14,11 @@ here="$PWD/$(dirname $0)"
 docker build -t "$IMAGE_TAG" "$here"
 source_archive="${NAME}_${version}.orig.tar.gz"
 git archive --format=tar --prefix "${NAME}-${version}/" "v${version}" | gzip > "$source_archive"
+# TODO: GPG sign
 docker run --rm \
   --volume "${here}/debian:/debian:ro" \
   --volume "${PWD}:/src:ro" \
   --volume "${here}:/output" \
   --workdir /build \
   "$IMAGE_TAG" \
-  # TODO: GPG sign
   bash -c "cp -a /debian . && ln -s /src/${source_archive} / && debuild -uc -us && cp -pv /*.deb /output/"
