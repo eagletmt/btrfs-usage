4685ac9a6262bb0e5132729d1a2d3cb2ac310c99
Kohei Suzuki <eagletmt@gmail.com>
2016-11-16 22:37:09 +0900

    Add packaging helper for Ubuntu xenial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # btrfs-usage
 Show detailed btrfs usage in machine-readable format
+
+## Packaging
+Ubuntu xenial
+
+```sh
+packaging/xenial/make.sh $VERSION
+```
---
+++ packaging/xenial/make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NAME=btrfs-usage
+IMAGE_TAG=ubuntu-xenial-packaging
+
+version="$1"
+if [ x"$version" = x"" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+set -ex
+here="$PWD/$(dirname $0)"
+docker build -t "$IMAGE_TAG" "$here"
+source_archive="${NAME}_${version}.orig.tar.gz"
+git archive --format=tar --prefix "${NAME}-${version}/" "v${version}" | gzip > "$source_archive"
+docker run --rm \
+  --volume "${here}/debian:/debian:ro" \
+  --volume "${PWD}:/src:ro" \
+  --volume "${here}:/output" \
+  --workdir /build \
+  "$IMAGE_TAG" \
+  # TODO: GPG sign
+  bash -c "cp -a /debian . && ln -s /src/${source_archive} / && debuild -uc -us && cp -pv /*.deb /output/"
